1b1f5f728553d7585219789bd5ab9193085be7fc
792268 <792268@SAIT225091IT.ACDM.DS.SAIT.CA>
2021-09-21 12:16:21 -0600

    New Project Creation Created a new Java Application for HelloWorld.

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,16 @@
+package helloworld;
+
+/**
+ *
+ * @author 792268
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
